3853075864b0d6db16df591e19a8cb7c696e2124
zaheer1213 <amolgithub123@gmail.com>
2023-02-03 16:09:32 +0530

    Admin.py added

--- sandip_portfolio/Portfolio/Portfolio_app/admin.py
+++ sandip_portfolio/Portfolio/Portfolio_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Projects,Certificate,Feedback
 # Register your models here.
+admin.site.register(Projects)
+admin.site.register(Certificate)
+admin.site.register(Feedback)
