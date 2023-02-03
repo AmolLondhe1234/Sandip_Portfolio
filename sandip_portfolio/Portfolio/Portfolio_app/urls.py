f5474f15bc0115415574e488930acffc0c97a3cb
Sandip Shimpi <sandipshimpi999@gmail.com>
2023-02-03 16:58:29 +0530

    app urls

--- sandip_portfolio/Portfolio/Portfolio_app/urls.py
+++ sandip_portfolio/Portfolio/Portfolio_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from Portfolio_app import views
 
 urlpatterns = [
-    path('',views.index,name='home')
-    
+    path('',views.index,name='home'),
+    path('cert',views.cert,name = 'cert'),
+    path('fb',views.fb,name='fb'),
+    path('home',views.home,name='home1'),
+    path('prjt',views.prjt,name='prjt'),
 ]
--- sandip_portfolio/Portfolio/Portfolio_app/views.py
+++ sandip_portfolio/Portfolio/Portfolio_app/views.py
@@ -16,8 +16,5 @@ def home(request):
 
     return render(request,'home.html')
 
-def index(request):
-    return render(request,'index.html')
-
 def prjt(request):
     return render(request,'project.html')
