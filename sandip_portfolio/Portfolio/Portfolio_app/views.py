e57c644e39c66a3a67360e870993391bbc4ca39c
Sandip Shimpi <sandipshimpi999@gmail.com>
2023-02-03 16:08:39 +0530

    views

--- sandip_portfolio/Portfolio/Portfolio_app/views.py
+++ sandip_portfolio/Portfolio/Portfolio_app/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
+    
     return render(request,'index.html')
+
+def cert(request):
+
+    return render(request,'certificate.html')
+
+def fb(request):
+
+    return render(request,'feedback.html')
+def home(request):
+
+    return render(request,'home.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def project(request):
+    return render(request,'project.html')
